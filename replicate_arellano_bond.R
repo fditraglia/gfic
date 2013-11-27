90c40eeb3c72014582871130ec7173a9d72c6fca
Francis DiTraglia <fditra@sas.upenn.edu>
2013-11-27 15:43:40 -0500

    Started script to replicate Arellano & Bond (1991)
    Just created a file and put in some documentation

---
+++ replicate_arellano_bond.R
@@ -0,0 +1,12 @@
+#------------------------------------------------------------
+#Filename:        replicate_arellano_bond.R
+#Author:          Frank DiTraglia
+#First Version:   2013-27-11
+#This Version:    2013-27-11
+#------------------------------------------------------------
+#This script is an attempt to replicate some of the results
+#from Arellano & Bond (1991): first using the plm package,
+#then using my own R code and finally using my own C++ code.
+#Once I'm sure everything is working correctly, I will then
+#incorporate this code into my GFIC simulation example.
+#------------------------------------------------------------
