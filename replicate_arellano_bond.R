01ac2180fc1fb64715cf5cbfdd555c27e23b4e8e
Francis DiTraglia <fditra@sas.upenn.edu>
2013-11-27 16:03:52 -0500

    plm to replicate Arellano & Bond (1991) Table 4b
    Point estimates agree although std errors do not. Next I'll try to code
    this up the hard way in R.

--- replicate_arellano_bond.R
+++ replicate_arellano_bond.R
@@ -8,5 +8,20 @@
 #from Arellano & Bond (1991): first using the plm package,
 #then using my own R code and finally using my own C++ code.
 #Once I'm sure everything is working correctly, I will then
-#incorporate this code into my GFIC simulation example.
+#incorporate this code into my GFIC simulation example. For
+#relevant details see Arellano & Bond (1991), the plm Package Vignette (Croissant & Millo), and Cameron & Trivedi (2005)
+#section 22.5.3.
 #------------------------------------------------------------
+
+library(plm) #R Package plm: Linear Models for Panel Data
+data(EmplUK) #UK Unemployment data from Arellano & Bond (1992)
+
+#Example from Section 5.4 of the plm package vignette
+#Replicates Table 4b of Arellano & Bond (1991)
+#``Employment is explained by past values of employment (two lags), current and first lag of wages and output and current value of capital.''
+emp.gmm <- pgmm(log(emp) ~ lag(log(emp), 1:2) + lag(log(wage), 0:1)
+           + log(capital) + lag(log(output), 0:1) | lag(log(emp), 2:99), data = EmplUK, effect = "twoways", model = "twosteps")
+
+#Estimates in the vignette agree with the paper, but standard errors are much bigger
+summary(emp.gmm)
+
