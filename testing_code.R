89dbf67ff94472d44ba6e99c721ccefcc1b4db4b
Francis DiTraglia <fditra@sas.upenn.edu>
2013-11-27 16:41:05 -0500

    Changes to testing_code.R
    Now that I've fully tested the C++ and R versions of the dgp function
    and found that: (1) they agree, (2) the C++ version is much faster,
    I've removed the corresponding testing code. From now on I'll always
    use the C++ version. In its place I've started adding code to test
    against the simulation study in Table 1 of Arellano & Bond (1991). So
    far all I've added is a wrapper that reproduces their DGP using my
    dgp_cpp function.

--- testing_code.R
+++ testing_code.R
@@ -4,30 +4,14 @@ sourceCpp("dgp.cpp")
 source("GFIC_arellano_bond.R")
 
 
-set.seed(837)
-fooR <- dgpR(a1 = 0.3, a2 = -0.1, g = 0.1, N.i = 25, N.t = 4, 
-             burn.in = 10, b = 1, r = 0.1, theta = 0.2, s.e = 1, 
-             s.eta = 1, s.v = 1)
 
-set.seed(837)
-fooCpp <- dgp_cpp(a1 = 0.3, a2 = -0.1, g = 0.1, N_i = 25, N_t = 4, 
-                  burn_in = 10, b = 1, r = 0.1, theta = 0.2, s_e = 1, 
-                  s_eta = 1, s_v = 1)
-
-all.equal(fooR$x, fooCpp$x)
-all.equal(fooR$y, fooCpp$y)
-
-
-library(microbenchmark)
-microbenchmark(
-  dgpR(a1 = 0.3, a2 = -0.1, g = 0.1, N.i = 25, N.t = 4, 
-       burn.in = 10, b = 1, r = 0.1, theta = 0.2, s.e = 1, 
-       s.eta = 1, s.v = 1),
-  dgp_cpp(a1 = 0.3, a2 = -0.1, g = 0.1, N_i = 25, N_t = 4, 
-          burn_in = 10, b = 1, r = 0.1, theta = 0.2, s_e = 1, 
-          s_eta = 1, s_v = 1)
-  )
-
+#This setup follows the "base design" of Arellano & Bond 1991 Table 1.
+AB_table1_sim <- function(a){
+  
+  dgp_cpp(a1 = a, a2 = 0, g = 0.1, N_i = 100, N_t = 7, 
+                 burn_in = 10, b = 1, r = 0.9, theta = 0, 
+                 s_e = sqrt(0.9), s_eta = 1, s_v = 1)
+}
 
 
 
