ed6a254a33159069d25dbd563ef970897f0f3db7
Francis DiTraglia <fditra@sas.upenn.edu>
2013-11-29 17:07:24 -0500

    Some progress on testing code
    Nearly have a working, although slow, Arellano-Bond estimator in R
    using sparse matrix operations and lapply. Still a couple of steps
    remaining and a few errors to fix.

--- testing_code.R
+++ testing_code.R
@@ -14,13 +14,14 @@ ABsim <- function(a){
 }
 
 
-set.seed(284)
+set.seed(2871)
 foo <- ABsim(0.5) #A draw from the DGP of Panel 1, Table 1, A&B 1991
 x <- foo$x
 y <- foo$y
 
 N.i <- nrow(x)
 N.t <- ncol(x)
+
 
 #Label rows and columns for sanity-checking
 rownames(y) <- rownames(x) <- paste('i=', 1:N.i, sep = '')
@@ -35,31 +36,47 @@ x.diff <- lapply(seq_len(N.i), function(i) x.diff[i,])
 y.diff <- lapply(seq_len(N.i), function(i) y.diff[i,])
 y <- lapply(seq_len(N.i), function(i) y[i,])
 
-#List of design matrices corresponding to each individual. Rows of each of the N.i dataframes correspond to time periods with NAs "padding" the missing time periods.
-data.list <- mapply(function(x.diff, y.diff, y) data.frame(x.diff = x.diff, y.diff = y.diff, y = y), x.diff, y.diff, y, SIMPLIFY = FALSE)
+#List of design matrices corresponding to each individual. Rows of each of the N.i dataframes correspond to time periods with NAs "padding" the missing time periods as a sanity check.
+data.list <- mapply(function(y.diff, x.diff, y) data.frame(y.diff = y.diff, x.diff = x.diff, y = y), x.diff, y.diff, y, SIMPLIFY = FALSE)
 
+#Instrument Matrix is block-diagonal. Construct using sparse matrices
+library(Matrix)
 
-#Function to handle a given individual. Construct as loop to avoid complicated matrix construction and storing really large sparse matrices
-XZ.i <- function(data.i){
-
-  out <- 0
-  x.diff <- data.i$x.diff
-  y.diff <- data.i$y.diff
-  y <- data.i$y
+#Instruments for person i
+Z.i <- function(i){
   
-  #Loop over time periods
-  for(j in 3:N.t){
-    
-    x.tilde.j <- c(y.diff[j - 1], x.diff[j])
-    z.j <- c(y[1:(j - 2)], x.diff[j])  
-    out <- out + x.tilde.j %*% t(z.j)#THIS IS NOT CORRECT!
-    
-  }
+  data.i <- data.list[[i]]
+  Z.i <- lapply(3:nrow(data.i), function(j) with(data.i, c(y[1:(j - 2)], x.diff[j])))
+  Z.i <- bdiag(Z.i)
+  Z.i <- t(Z.i)
+  
+}
+
+#Function for a given individual
+XZ.i <- function(i){
+  
+  data.i <- data.list[[i]]
+  Z.i <- lapply(3:nrow(data.i), function(j) with(data.i, c(y[1:(j - 2)], x.diff[j])))
+  
+  Z.i <- bdiag(Z.i)
+  Z.i <- t(Z.i)
+  
+  X.tilde.i <- as.matrix(data.i[3:nrow(data.i), c("x.diff", "y.diff")])
+  XZ.i <- crossprod(X.tilde.i, Z.i)
+  return(XZ.i)
   
 }
 
 
+XZ <- lapply(seq_len(N.i), XZ.i)
+XZ <- Reduce('+', XZ) #Sum over all individuals
 
+H <- bandSparse(5, 5, k = c(0,-1, 1), list(rep(2, 5), rep(-1, 5), rep(-1, 5)))
+Z <- lapply(seq_len(N.i), Z.i)
+W.n <- lapply(seq_len(N.i), function(i) t(Z[[i]]) %*% H %*% Z[[i]])
+W.n <- Reduce('+', W.n)
+W.n <- W.n / N.i
+W.n <- solve(W.n)
 
-
+solve(XZ %*% W.n %*% t(XZ))
 
