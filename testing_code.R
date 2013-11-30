b709d49fb353329ed4fdcc3df66e587790524314
Francis DiTraglia <fditra@sas.upenn.edu>
2013-11-29 19:13:30 -0500

    First replication of Arellano & Bond (1991) Simulation Table 1
    I added the summary statistics inline since the simulations take a
    while to run. The results make qualitative sense but my estimators of
    beta are biased downwards by a fair bit. There are a few possibilities:
    (1) something is wrong with the results in the original paper, (2)
    dgp.cpp code is somehow different from the DGP used in the paper, (3)
    there's a mistake in my code for the estimator. I can try to rule out
    (3) by going back over everything carefully as I rewrite in C++ to try
    to speed things up. Should also go over the dgp function...

--- testing_code.R
+++ testing_code.R
@@ -5,11 +5,63 @@ sourceCpp("dgp.cpp")
 source("GFIC_arellano_bond.R")
 
 
+#Function to replicate one draw from the simulation experiment reported in the first column of Table 1 in Arellano & Bond (1991)
+#NOTE: this code is slow! Takes about 0.7 seconds to run...
+test.sim <- function(a){
+  
+  sim.data <- ABsim(a)
+  ABfit(x = sim.data$x, y = sim.data$y)
+  
+}
+
+
 set.seed(871)
-foo <- ABsim(0.5) #A draw from the DGP of Panel 1, Table 1, A&B 1991
-x <- foo$x
-y <- foo$y
-ABfit(x, y)
+
+#Simulations corresponding to alpha = 0.2, 0.5, 0.8
+#Each of these takes a little over a minute. I really need to speed things up...
+sim.2 <- replicate(100, test.sim(0.2))
+sim.5 <- replicate(100, test.sim(0.5))
+sim.8 <- replicate(100, test.sim(0.8))
+
+a.sim.2 <- sapply(seq_len(100), function(i) sim.2[[i]][1,1], simplify = "array")
+mean(a.sim.2)
+#[1] 0.219452
+sd(a.sim.2)
+#[1] 0.08032777
+
+b.sim.2 <- sapply(seq_len(100), function(i) sim.2[[i]][2,1], simplify = "array")
+mean(b.sim.2)
+#[1] 0.9022707
+sd(b.sim.2)
+#[1] 0.07124266
+
+
+a.sim.5 <- sapply(seq_len(100), function(i) sim.5[[i]][1,1], simplify = "array")
+mean(a.sim.5)
+#[1] 0.458137
+sd(a.sim.5)
+#[1] 0.09586758
+
+b.sim.5 <- sapply(seq_len(100), function(i) sim.5[[i]][2,1], simplify = "array")
+mean(b.sim.5)
+#[1] 0.9258031
+sd(b.sim.5)
+#[1] 0.06598999
+
+
+a.sim.8 <- sapply(seq_len(100), function(i) sim.8[[i]][1,1], simplify = "array")
+mean(a.sim.8)
+#[1] 0.7582259
+sd(a.sim.8)
+#[1] 0.05669517
+
+b.sim.8 <- sapply(seq_len(100), function(i) sim.8[[i]][2,1], simplify = "array")
+mean(b.sim.8)
+#[1] 0.9045425
+sd(b.sim.8)
+#[1] 0.06523568
+
+
 
 
 
